8b6bacf53541869d3873b177f4590d4180f77a73
Iurii Gazin <archeg@gmail.com>
2014-11-09 20:13:46 +0200

    Implemented DB layer

--- server/src/wol/db.py
+++ server/src/wol/db.py
@@ -13,42 +13,96 @@ class DbService():
         """
         self.db = db
         self.usertable = self.db['UserTable']
+        self.auth_timeout = datetime.timedelta(minutes=20)
 
-    def authenticate(self, login, user_id):
-        """Creates a record for the user or changes it's user_id"""
-        storedlogin = self.usertable.find_one({"login": login}, {"uuid": 1, "user_id": 1})
+    def authenticate(self, login, pwd, device_id):
+        """
+        Authenticates the user to be able to work with the server
+        @returns: token to be used when talking to the server.
+        """
+        from passlib.hash import pbkdf2_sha256
+        storedlogin = self.usertable.find_one({"login": login}, {"tokens": 1, "devices": 1, "pwd": 1})
         if not storedlogin:
             # create a record
             storedloginid = self.usertable.insert(
                 {
-                    "login" : login,
-                    "friends": [],
-                    "user_id": user_id,
-                    "date": datetime.datetime.utcnow(),
-                    "uuid": uuid.uuid1()
+                    "login": login,
+                    "devices": [device_id],
+                    "pwd": pbkdf2_sha256.encrypt(pwd, rounds=20000, salt_size=16),
+                    "registered_date": datetime.datetime.utcnow(),
+                    "reg_ids": {},
+                    "tokens": {device_id: {"token": uuid.uuid1(), "logintime": datetime.datetime.utcnow()}}
                 })
+
             storedlogin = self.usertable.find_one({"_id": storedloginid})
 
             self.usertable.ensure_index("login", unique=1)
+        else:
+            # quit if password is incorrect
+            if not pbkdf2_sha256.verify(pwd, storedlogin["pwd"]):
+                return False
 
-        if user_id != storedlogin['user_id']:
-            # Update user id if it is expired
-            self.usertable.update({"login": login}, {"$set": {"user_id": user_id}})
+            # Add device
+            if device_id not in storedlogin['devices']:
+                generated_token = uuid.uuid1()
+                # Update user id if it is expired
+                self.usertable.update({"login": login},
+                                      {
+                                          # Add device to the list
+                                          "$addToSet": {"devices": device_id},
+                                          # Add token and current login time
+                                          "$set": {"tokens." + device_id: {"token": generated_token, "logintime": datetime.datetime.utcnow()}}
+                                      })
+                return generated_token
 
-        return storedlogin['uuid']
+        return storedlogin['tokens'][device_id]["token"]
 
-    def add_friend(self, login, friendlogin):
-        """ Adds a friend to the user """
+    def update_regid(self, login, device_id, regid):
+        """
+        Renews google reg id record
+        """
+        self.usertable.update({"login": login}, {
+            "$set": {"reg_ids."+device_id: regid}
+        })
 
-        self.usertable.update({"login": login}, {"$addToSet": {"friends": friendlogin}})
+    def get_regid(self, login, deviceid):
+        """
+        @return regid from the database.
+        """
+        record = self.usertable.find_one({"login": login}, {"reg_ids": 1})
+        return record["reg_ids"][deviceid]
 
-    def get_friends_user_ids(self, login, friendslogins):
-        """ Returns the user_ids for the friend login`s, or throws if one o the friends does not have a permission """
+    def check_token(self, login, device_id, token):
+        """
+        Checks token for being authenticated.
+        """
+        #self.check_timeout(login)
+        storedlogin = self.usertable.find_one(
+            {
+                "login": login,
+                "devices": {"$elemMatch": {"$eq": device_id}},
+                "tokens."+device_id+".token": token
+            })
 
-        # Check permissions
-        allowedfriends = self.usertable.find_one({"login": login}, {"friends": 1}).friends
-        for friend in friendslogins:
-            if friend not in allowedfriends:
-                raise Exception('No permission for %s' % friend)
+        if storedlogin:
+            return True
+        return False
 
-        return [self.usertable.find_one({"login": x}, {"user_id": 1})['user_id'] for x in friendslogins]
+    def check_timeout(self, login):
+        """
+        Checks given login for possible expired devices (they should re-authenticate from time to time)
+        """
+        storedlogin = self.usertable.find_one({"login": login}, {"tokens": 1})
+        expired_devices = set()
+        for device_id, details in storedlogin["tokens"].iteritems():
+            logintime = details["logintime"]
+            if logintime + self.auth_timeout < datetime.datetime.utcnow():
+                expired_devices.add(device_id)
+
+        # Removing expired devices
+        for device_id in expired_devices:
+            self.usertable.update({"login": login},
+                {
+                    "$pull": {"devices": device_id},
+                    "$unset": {"tokens."+device_id: 1}
+                })
--- server/tests/dbService-Test.py
+++ server/tests/dbService-Test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from unittest import *
+import datetime
 from pymongo import MongoClient
 from db import DbService
 
@@ -12,31 +13,130 @@ class TestDbService(TestCase):
         self.client = MongoClient()
         self.client.drop_database("TestWOL")
         self.db = self.client['TestWOL']
-        self.dbClient = DbService(self.db)
-        self.userTable = self.db['UserTable']
+        self.dbclient = DbService(self.db)
+        self.usertable = self.db['UserTable']
+        if self.usertable.count() != 0:
+            self.fail("TestWOL was not clean-up before running test")
 
     def test_Authenticate(self):
+        from passlib.hash import pbkdf2_sha256
         login = "archeg@gmail.com"
-        id = "uuser_id_arch"
+        pwd = "1234"
+        device_id = "HD1"
 
-        self.assertFalse(self.userTable.find_one({"login": login}))
-        self.dbClient.authenticate(login, id)
-        foundUser = self.userTable.find_one({"login": login})
-        self.assertTrue(foundUser)
-        self.assertEqual(foundUser['user_id'], id)
+        self.assertFalse(self.usertable.find_one({"login": login}))
+        token = self.dbclient.authenticate(login, pwd, device_id)
+        found_user = self.usertable.find_one({"login": login})
+        self.assertTrue(token)
+        self.assertTrue(found_user)
+        self.assertEqual(found_user['login'], login)
+        self.assertTrue(pbkdf2_sha256.verify(pwd, found_user['pwd']))
+        self.assertTrue(device_id in found_user['devices'])
+        self.assertEqual(found_user['tokens'][device_id]['token'], token)
+        token2 = self.dbclient.authenticate(login, pwd, device_id)
+        self.assertEqual(token, token2)
 
-    def test_Authenticate_double(self):
+    def test_Authenticate_wrong_pwd(self):
+        from passlib.hash import pbkdf2_sha256
+
         login = "archeg@gmail.com"
-        id = "uuser_id_arch"
+        pwd = "1234"
+        device_id = "HD1"
 
-        self.dbClient.authenticate(login, id)
-        self.assertEqual(id, self.userTable.find_one({"login": login})['user_id'])
-        self.dbClient.authenticate(login, id)
+        self.assertFalse(self.usertable.find_one({"login": login}))
+        self.dbclient.authenticate(login, pwd, device_id)
+        token = self.dbclient.authenticate(login, "1235", device_id)
+        self.assertFalse(token)
+        found_user = self.usertable.find_one({"login": login})
+        self.assertTrue(pbkdf2_sha256.verify(pwd, found_user['pwd']))
 
-    def test_AddFriend(self):
-        pass
-        #self.fail()
+    def test_Authenticate_two_devices(self):
+        login = "archeg@gmail.com"
+        pwd = "1234"
+        device_id1 = "HD1"
+        device_id2 = "HD2"
 
-    def test_GetFriendsUserIds(self):
-        pass
-        #self.fail()
+        self.assertFalse(self.usertable.find_one({"login": login}))
+        token = self.dbclient.authenticate(login, pwd, device_id1)
+        token2 = self.dbclient.authenticate(login, pwd, device_id2)
+        self.assertNotEqual(token, token2)
+
+        found_user = self.usertable.find_one({"login": login})
+        self.assertTrue(token)
+        self.assertTrue(token2)
+        self.assertTrue(found_user)
+        self.assertEqual(found_user['login'], login)
+        self.assertTrue(device_id1 in found_user['devices'])
+        self.assertTrue(device_id2 in found_user['devices'])
+        self.assertEqual(found_user['tokens'][device_id1]['token'], token)
+        self.assertEqual(found_user['tokens'][device_id2]['token'], token2)
+
+    def test_checktoken_success(self):
+        login = "archeg@gmail.com"
+        dev = "dev1"
+        token = self.dbclient.authenticate(login, "1234", dev)
+        result = self.dbclient.check_token(login, dev, token)
+        self.assertTrue(result)
+
+    def test_checktoken_fail(self):
+        login = "archeg@gmail.com"
+        dev = "dev1"
+        token = self.dbclient.authenticate(login, "1234", dev)
+        result = self.dbclient.check_token(login, dev, "sadvb")
+        result2 = self.dbclient.check_token("faillogin@gmail.com", dev, token)
+        result3 = self.dbclient.check_token(login, "dev2", token)
+        self.assertFalse(result)
+        self.assertFalse(result2)
+        self.assertFalse(result3)
+
+    def test_update_regid(self):
+        login = "archeg@gmail.com"
+        dev = "dev1"
+        dev2 = "dev2"
+        token = self.dbclient.authenticate(login, "1234", dev)
+        self.dbclient.update_regid(login, dev, "AA")
+        self.assertEqual("AA", self.usertable.find_one({"login": login})["reg_ids"][dev])
+        self.dbclient.update_regid(login, dev, "AB")
+        self.assertEqual("AB", self.usertable.find_one({"login": login})["reg_ids"][dev])
+
+        #updating different device
+        self.dbclient.update_regid(login, dev2, "AC")
+        self.assertEqual("AB", self.usertable.find_one({"login": login})["reg_ids"][dev])
+        self.assertEqual("AC", self.usertable.find_one({"login": login})["reg_ids"][dev2])
+
+    def test_get_regid(self):
+        login = "archeg@gmail.com"
+        login2 = "john@gmail.com"
+        dev = "dev1"
+        dev2 = "dev2"
+        dev3 = "dev3"
+        dev4 = "dev2"
+        token = self.dbclient.authenticate(login, "1234", dev)
+        token = self.dbclient.authenticate(login2, "1234", dev3)
+        self.dbclient.update_regid(login, dev, "AB")
+        self.dbclient.update_regid(login, dev2, "AC")
+        self.dbclient.update_regid(login2, dev3, "AD")
+        self.dbclient.update_regid(login2, dev4, "AE")
+
+        self.assertEqual("AB", self.dbclient.get_regid(login, dev))
+        self.assertEqual("AC", self.dbclient.get_regid(login, dev2))
+        self.assertEqual("AD", self.dbclient.get_regid(login2, dev3))
+        self.assertEqual("AE", self.dbclient.get_regid(login2, dev4))
+
+    def test_checktimeout(self):
+        login = "archeg@gmail.com"
+        dev = "dev1"
+        token = self.dbclient.authenticate(login, "1234", dev)
+        self.assertTrue(self.dbclient.check_token(login, dev, token))
+        self.dbclient.check_timeout(login)
+        self.assertTrue(self.dbclient.check_token(login, dev, token))
+
+        # change time to be quite a while ago
+        self.usertable.update({"login": login}, {"$set": {"tokens.dev1.logintime": datetime.datetime.utcnow() - datetime.timedelta(days=1)}})
+        self.assertTrue(self.dbclient.check_token(login, dev, token))
+        self.dbclient.check_timeout(login)
+        self.assertFalse(self.dbclient.check_token(login, dev, token))
+
+        # the token can be renewed
+        token = self.dbclient.authenticate(login, "1234", dev)
+        self.assertTrue(self.dbclient.check_token(login, dev, token))
